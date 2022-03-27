82706933c682a596236327a12c365b47db7b7276
wezleyp <wesley.patterson121@gmail.com>
2022-03-27 13:56:30 -0500

    Initial Commit

---
+++ BeginnerProject-2Task/BeginnerProject_2TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginnerProject_2TaskApp.swift
+//  BeginnerProject-2Task
+//
+//  Created by Wesley Patterson on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct BeginnerProject_2TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
